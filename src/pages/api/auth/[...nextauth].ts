04750d597b92b69ffc311266234f6ee1ad703ce7
Ysrael Moreno <ysraelmoreno02@gmail.com>
2023-04-09 16:18:36 -0300

    fix(next-auth): fix return type (#7)
    Co-authored-by: Ysrael Moreno <ysrael.moreno@ab-inbev.com>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { Awaitable } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { JwtPayload, sign, verify } from 'jsonwebtoken';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -37,10 +37,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     secret: '9909a8c25150df1947166b5aa5d1e8bd409a06af5354b6bd2b803394cb94f16c',
 
     jwt: {
-      encode: async ({ secret, token }) => {
+      encode: ({ secret, token }): Awaitable<string> => {
         if (token?.token) return token?.token as string;
 
-        return req.cookies['next-auth.session-token'];
+        return req.cookies['next-auth.session-token'] as Awaitable<string>;
       },
       decode: async ({ token, secret }) => {
         if (!token) {
